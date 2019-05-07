861f4a4a2169036fb8dd793a92da2c7f6f0ef892
Hugh O'Reilly <horeilly1101@gmail.com>
2019-05-06 23:08:42 -0500

    fixed mlh logo

--- components/mlh.js
+++ components/mlh.js
@@ -1,46 +1,31 @@
 import React from "react";
 
-import { Image } from "rebass";
+const linkStyle = {
+    display : "block",
+    maxWidth : "100px",
+    minWidth : "60px",
+    position : "fixed",
+    right : "50px",
+    top : "0",
+    width : "10%",
+    zIndex : "10000"
+};
 
+const imgStyle = {
+    width : "100%"
+};
+
+/**
+ * React component that places an MLH logo in the top right corner of the
+ * screen. Provided by mlh. See: https://mlh.io/brand-guidelines
+ *
+ * @returns {React.Component}
+ * @constructor
+ */
 const Mlh = () => (
-    <nav>
-        <ul>
-            <ul>
-                <li>
-                    {/* a hack, but it gets the job done */}
-                </li>
-            </ul>
-            <ul>
-                <Image src="../static/mlh.svg"
-                       css={{
-                           height: "105px !important",
-                           paddingRight: "20px"
-                       }}
-                       ml={10}
-                />
-            </ul>
-        </ul>
-        <style jsx>{`
-      nav {
-        text-align: center;
-        margin-top: 0px;
-      }
-
-      ul {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding-left: 0px;
-        margin-top: 0px;
-      }
-
-      li {
-        display: flex;
-        flex: 1;
-        margin-top: 0px;
-      }
-    `}</style>
-    </nav>
+    <a id="mlh-trust-badge" style={linkStyle} href="https://mlh.io/seasons/na-2020/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2020-season&utm_content=gray" target="_blank">
+        <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2020/mlh-trust-badge-2020-gray.svg" alt="Major League Hacking 2020 Hackathon Season" style={imgStyle} />
+    </a>
 );
 
 export default Mlh;
--- docs/_next/static/commons/manifest.js
+++ docs/_next/static/commons/manifest.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "6f96f846a9c01d51d0ce"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "6f7afcc3155ce3fff732"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
