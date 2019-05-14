2a5a92f9fb3e23d89d5d3f6237964303ba26f720
Hugh O'Reilly <horeilly1101@gmail.com>
2019-05-14 17:56:52 -0500

    added subscribe button for mlh

--- content/cardNav.js
+++ content/cardNav.js
@@ -4,10 +4,10 @@
  */
 export const cards = [
   {
-      icon: "https://icon.now.sh/compose/ffffff",
-      title: "Register",
-      href: "https://hr9-registration.herokuapp.com",
-      text: "Registration is open for students outside of Rice! Apply now →"
+    icon: "https://icon.now.sh/compose/ffffff",
+    title: "Register",
+    href: "https://hr9-registration.herokuapp.com",
+    text: "Registration is open for students outside of Rice! Apply now →"
   },
   {
     icon: "https://icon.now.sh/send/ffffff",
@@ -20,6 +20,12 @@ export const cards = [
     title: "Contact",
     href: "mailto:hack@rice.edu",
     text: "Have a question? Reach out via email →"
+  },
+  {
+    icon: "https://icon.now.sh/check/ffffff",
+    title: "Subscribe",
+    href: "https://forms.gle/qhbkpBJdJT59Tj3T8",
+    text: "Follow HackRice and receive updates, reminders, and more →"
   }
 ].map(card => {
   card.key = `card-${card.title}`;
