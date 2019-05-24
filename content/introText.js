8fd94ecbe74adb7af5d316bf7e317d1e684166f7
Hugh O'Reilly <horeilly1101@gmail.com>
2019-05-24 17:25:47 -0500

    update intro

--- content/introText.js
+++ content/introText.js
@@ -2,7 +2,7 @@
  * The text that appears at the top of the hackrice web page.
  * @type {string}
  */
-export const intro = "HackRice 9: Coming Soon...";
+export const intro = "HackRice 9: Apply now!";
 
 /**
  * A list of bullet points that introduce the event.
