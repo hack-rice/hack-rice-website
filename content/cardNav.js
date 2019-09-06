15586d61e8b2e2fccd3ebebfa652d2dc9cfd6d9c
Hugh O'Reilly <horeilly1101@gmail.com>
2019-09-05 19:34:05 -0500

    link registration form

--- content/cardNav.js
+++ content/cardNav.js
@@ -5,7 +5,13 @@
 export const cards = [
   {
     icon: "https://icon.now.sh/compose/ffffff",
-    title: "Register",
+    title: "Rice Student Registration",
+    href: "https://forms.gle/wLN8cmDXp9nnJ18q6",
+    text: "Are you a Rice student? Register for HackRice 9 →"
+  },
+  {
+    icon: "https://icon.now.sh/compose/ffffff",
+    title: "(Outside of Rice) Portal",
     href: "https://hr9-registration.herokuapp.com",
     text: "Registration has closed for students outside Rice! Visit our registration portal →"
   },
