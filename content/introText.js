40f6f1e77495c9298435a3fe0103b3b9457cc74d
Hugh O'Reilly <horeilly1101@gmail.com>
2019-09-22 16:36:39 -0500

    that 's it for this year

--- content/introText.js
+++ content/introText.js
@@ -2,7 +2,7 @@
  * The text that appears at the top of the hackrice web page.
  * @type {string}
  */
-export const intro = "HackRice 9: We Are NOT Cancelled!";
+export const intro = "HackRice 9: See ya next year!";
 
 /**
  * A list of bullet points that introduce the event.
