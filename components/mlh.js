01076875098ac3d8b42c0b76c515bc10dd5d3a76
“edixiong” <xiongzheyang@gmail.com>
2021-08-06 02:58:04 +0800

    added badget

--- components/mlh.js
+++ components/mlh.js
@@ -24,11 +24,12 @@ const imgStyle = {
  */
 const Mlh = () => (
     <a id="mlh-trust-badge"
-       style={linkStyle} href="https://mlh.io/seasons/na-2021/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=white"
-       target="_blank">
-        <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-white.svg"
-             alt="Major League Hacking 2021 Hackathon Season"
-             style={imgStyle} />
+      style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000"
+      href="https://mlh.io/seasons/2022/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white"
+      target="_blank">
+        <img src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-white.svg"
+          alt="Major League Hacking 2022 Hackathon Season"
+          style="width:100%">
     </a>
 );
 
